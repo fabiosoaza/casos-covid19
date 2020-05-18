0d7eaff4a24b105b822304ad009594d1f795ef00
Fábio Souza <fabio.souza.eti@gmail.com>
2020-05-18 00:48:09 -0300

    Ajustes progress bar e nos textos em inglês

--- app/src/main/java/com/github/fabiosoaza/casos/covid19/ui/ProgressBarComponent.kt
+++ app/src/main/java/com/github/fabiosoaza/casos/covid19/ui/ProgressBarComponent.kt
@@ -8,11 +8,13 @@ import java.util.concurrent.atomic.AtomicBoolean
 
 class ProgressBarComponent (private var view: ComponentActivity) {
 
-    var progressOverlay: View = view.findViewById(R.id.progressOverlay)
-    var showingProgressBar : AtomicBoolean = AtomicBoolean(false)
+    private var container: View = view.findViewById(R.id.container)
+    private var progressOverlay: View = view.findViewById(R.id.progressOverlay)
+    private var showingProgressBar : AtomicBoolean = AtomicBoolean(false)
 
     fun show(){
         if(showingProgressBar.compareAndSet(false, true)){
+            container.visibility=View.GONE
             UiUtil.sendTalkBackMessage(view.baseContext, view.getString(R.string.labelDownloadingData))
             UiUtil.animateView(progressOverlay, View.VISIBLE, 0.4F, 200)
         }
@@ -20,6 +22,7 @@ class ProgressBarComponent (private var view: ComponentActivity) {
 
     fun hide(){
         if(showingProgressBar.compareAndSet(true, false)) {
+            container.visibility=View.VISIBLE
             UiUtil.sendTalkBackMessage(view.baseContext, view.getString(R.string.labelDownloadingDataComplete))
             UiUtil.animateView(progressOverlay, View.GONE, 0F, 200)
         }
